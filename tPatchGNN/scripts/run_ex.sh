45bd49d93a2f6d81213073d7334ea971756ef781
PTAEJOOO <qkrxown5985@gmail.com>
2025-01-14 16:33:19 +0900

    sc

--- tPatchGNN/scripts/run_ex.sh
+++ tPatchGNN/scripts/run_ex.sh
@@ -6,5 +6,6 @@ python run_models.py \
     --dataset physionet --state 'def' --history 24 \
     --patience $patience --batch_size 32 --lr 1e-3 \
     --patch_size 8 --stride 8 --nhead 1 --tf_layer 1 --nlayer 1 \
+    --patch_size_ 2 --stride_ 2 \
     --te_dim 10 --node_dim 10 --hid_dim 64 \
     --outlayer Linear --seed 0 --gpu $gpu
