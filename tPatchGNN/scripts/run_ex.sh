1c2b7c264b5a042910b2349a6b15c667f9200825
PTAEJOOO <qkrxown5985@gmail.com>
2025-01-21 21:05:19 +0900

    tpatchgnn: run on GPU server

--- tPatchGNN/scripts/run_ex.sh
+++ tPatchGNN/scripts/run_ex.sh
@@ -1,10 +1,15 @@
 ### tPatchGNN ###
-patience=10
-gpu=0
+export CUDA_VISIBLE_DEVICES=1
 
-python run_models.py \
-    --dataset physionet --state 'def' --history 24 \
-    --patience $patience --batch_size 32 --lr 1e-3 \
-    --patch_size 8 --stride 8 --nhead 1 --tf_layer 1 --nlayer 1 \
-    --te_dim 10 --node_dim 10 --hid_dim 64 \
-    --outlayer Linear --seed 0 --gpu $gpu
+patience=10
+seed="0 1 2 3 4 5"
+gpu=1
+
+for sd in $seed;do
+    python run_models.py \
+        --dataset physionet --state 'def' --history 24 \
+        --patience $patience --batch_size 32 --lr 1e-3 \
+        --patch_size 8 --stride 8 --nhead 1 --tf_layer 1 --nlayer 1 \
+        --te_dim 10 --node_dim 10 --hid_dim 64 \
+        --outlayer Linear --seed $sd --gpu $gpu
+done
